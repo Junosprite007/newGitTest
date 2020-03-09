e233eebe481c9de94ed66104047e328680be2e2c
BuildTools <unconfigured@null.spigotmc.org>
2020-03-09 18:05:11 -0400

    Simple Example of using argparse

--- HelloWorld.py
+++ HelloWorld.py
@@ -7,6 +7,5 @@ parser.add_argument('-n','--name',help='Please Enter Your Name',required=True)
 parser.add_argument('-num','--number',help='Please Enter a number',required=True)
 args=parser.parse_args()
 
-while(int(args.number)>0):
-        print('Hello %s' %(args.name))
+print('Hello %s\n' %(args.name)*int(args.number))
 
--- README.md
+++ README.md
@@ -2,4 +2,7 @@
 This is a set of random scripts that will not amount to anything useful.
 
 ## HelloWorld.py
-Example Usage: ./HelloWorld.py -n name -num int 
+A simple example of using the argparse library in python
+
+#### Example Usage: 
+./HelloWorld.py -n name -num int 
